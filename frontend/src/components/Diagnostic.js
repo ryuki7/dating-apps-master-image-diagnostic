d41c739b0be5f469404a4c3adc1cfab2637cd9c6
ryuki7 <ryuki111209@i.softbank.jp>
2022-04-06 20:30:52 +0900

    [Add]トップページの作成

---
+++ frontend/src/components/Diagnostic.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Diagnostic() {
+    return (
+        <h2>診断ページ</h2>
+    )
+}
+
+export default Diagnostic;
---
+++ frontend/src/components/NotFound404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NotFound404() {
+    return (
+      <h2>Not Found Page</h2>
+    )
+}
+
+export default NotFound404;
